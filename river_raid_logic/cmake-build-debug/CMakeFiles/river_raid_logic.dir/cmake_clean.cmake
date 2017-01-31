5780fdc822db8c0fa70d2a665ce208b06bba499b
mo-mahdavi <mobina.mahdavi@gmail.com>
2017-01-31 10:54:03 +0330

    Add files via upload

---
+++ river_raid_logic/cmake-build-debug/CMakeFiles/river_raid_logic.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/river_raid_logic.dir/main.cpp.o"
+  "CMakeFiles/river_raid_logic.dir/Item.cpp.o"
+  "river_raid_logic.pdb"
+  "river_raid_logic.exe"
+  "libriver_raid_logic.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/river_raid_logic.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
